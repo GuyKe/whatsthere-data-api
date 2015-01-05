6c2cbff670aac8cc43b72bebb27851881980b6d8
Guy <guy.keren@live.com>
2015-01-05 21:18:02 +0200

    changed the response

--- src/main/java/com/whatsthere/api/data/TagImagesMap.java
+++ src/main/java/com/whatsthere/api/data/TagImagesMap.java
@@ -1,11 +1,6 @@
 package com.whatsthere.api.data;
 
-import com.whatsthere.api.transformers.ToJsonTransformer;
-
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 
 import static com.google.gson.internal.$Gson$Preconditions.checkNotNull;
 
@@ -13,20 +8,20 @@ import static com.google.gson.internal.$Gson$Preconditions.checkNotNull;
  * Created by guyk on 1/4/15.
  */
 public class TagImagesMap {
-   private HashMap<String,Images> map;
+   private HashMap<String,Images> hashTag;
 
     public void addTagImagesMap(Images images){
         checkNotNull(images);
-        map = new HashMap<String, Images>();
+        hashTag = new HashMap<String, Images>();
         for(int i =0 ; i<images.getImagesLength(); i++)
         {
-            if(map.containsKey(images.getImage(i).getHashTagText())) {
-                map.get(images.getImage(i).getHashTagText()).addImage(images.getImage(i));
+            if(hashTag.containsKey(images.getImage(i).getHashTagText())) {
+                hashTag.get(images.getImage(i).getHashTagText()).addImage(images.getImage(i));
             }
             else {
                 Images imageList  = new Images();
                 imageList.addImage(images.getImage(i));      ;
-                map.put(images.getImage(i).getHashTagText(), imageList);
+                hashTag.put(images.getImage(i).getHashTagText(), imageList);
             }
         }
     }
